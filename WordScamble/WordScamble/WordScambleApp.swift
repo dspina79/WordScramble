24fa198aab374b94e009e1f344755bba567384ed
Dave Spina <dspina@nimbleuser.com>
2020-11-29 10:46:00 -0500

    Basic application creation

---
+++ WordScamble/WordScamble/WordScambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScambleApp.swift
+//  WordScamble
+//
+//  Created by Dave Spina on 11/29/20.
+//
+
+import SwiftUI
+
+@main
+struct WordScambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
