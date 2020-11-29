80640373df20ea22b0e05119db7ad8c918b6faaa
Dave Spina <dspina@nimbleuser.com>
2020-11-29 16:42:36 -0500

    Mispelling handling

--- WordScamble/WordScamble/ContentView.swift
+++ WordScamble/WordScamble/ContentView.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         let people = ["John", "Paul", "George", "Ringo"]
-        
         // file URL usage
         if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
             if let fileContent = try? String(contentsOf: fileURL) {
@@ -18,8 +18,24 @@ struct ContentView: View {
             }
             
         }
+        // handling mispellings
+        let word = "wastekdkd"
+        let checker = UITextChecker()
+
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let mispelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+       let mispellingResult = mispelledRange.location == NSNotFound ? "All good" : "Oops!"
+        
+        let inputLetters = """
+                            a
+                            b
+                            c
+                            dave
+                            """
+        let arrayLetters = inputLetters.components(separatedBy: "\n")
         
         List {
+            Text("Mispelling Message: \(mispellingResult)")
             Section(header: Text("Dynamic Data")) {
                 ForEach(people, id: \.self) {
                     Text($0)
@@ -27,6 +43,15 @@ struct ContentView: View {
             }
             Section(header: Text("Static Data")) {
                 Text("And Bobby!")
+            }
+            Section(header: Text("Data from String")) {
+                ForEach(arrayLetters, id: \.self) {
+                    Text($0)
+                }
+            }
+            // random letter
+            Section(header: Text("Random Letter")) {
+                Text("\(arrayLetters.randomElement() ?? "")")
             }
         }
     }
