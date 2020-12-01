7a4478fa1cfbbd7e15e6e75b03b8e0be9b985501
Dave Spina <dspina@nimbleuser.com>
2020-11-30 21:03:59 -0500

    Implemented logic of project

--- WordScamble/WordScamble/ContentView.swift
+++ WordScamble/WordScamble/ContentView.swift
@@ -8,52 +8,107 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
     
     var body: some View {
-        let people = ["John", "Paul", "George", "Ringo"]
-        // file URL usage
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            if let fileContent = try? String(contentsOf: fileURL) {
-                // the file contents are loaded into a string
+        NavigationView {
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [.blue, .green, .yellow]), startPoint: .top, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                VStack {
+                    TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                        .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                    List(usedWords, id: \.self) {
+                        Image(systemName: "\($0.count).circle")
+                        Text($0)
+                    }
+                    .padding()
+                    Spacer()
+                    
+                }.navigationBarTitle(rootWord)
+                .onAppear(perform: startGame)
+                .alert(isPresented: $showingError) {
+                    Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("Ok")))
+                }
             }
-            
         }
-        // handling mispellings
-        let word = "wastekdkd"
+    }
+    
+    func isReal(word: String) -> Bool {
         let checker = UITextChecker()
-
         let range = NSRange(location: 0, length: word.utf16.count)
-        let mispelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-       let mispellingResult = mispelledRange.location == NSNotFound ? "All good" : "Oops!"
+        let misspelledWords = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         
-        let inputLetters = """
-                            a
-                            b
-                            c
-                            dave
-                            """
-        let arrayLetters = inputLetters.components(separatedBy: "\n")
-        
-        List {
-            Text("Mispelling Message: \(mispellingResult)")
-            Section(header: Text("Dynamic Data")) {
-                ForEach(people, id: \.self) {
-                    Text($0)
-                }
-            }
-            Section(header: Text("Static Data")) {
-                Text("And Bobby!")
-            }
-            Section(header: Text("Data from String")) {
-                ForEach(arrayLetters, id: \.self) {
-                    Text($0)
-                }
-            }
-            // random letter
-            Section(header: Text("Random Letter")) {
-                Text("\(arrayLetters.randomElement() ?? "")")
+        return misspelledWords.location == NSNotFound
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord.lowercased()
+        for letter in word {
+            if let pos = tempWord.firstIndex(of:  letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
             }
         }
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func startGame() {
+        if let wordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: wordsURL) {
+                let wordsArray = startWords.components(separatedBy: "\n")
+                rootWord = wordsArray.randomElement() ?? "balderdash"
+                return
+            }
+        }
+        
+        fatalError("Could not load dictionary file.")
+    }
+    
+    func wordError(title: String, message: String) {
+        self.errorTitle = title
+        self.errorMessage = message
+        self.showingError = true
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else {
+            wordError(title: "Empty", message: "You cannot provide an empty word.")
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Not Original", message: "You've already specified that word.")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Not Possible", message: "You need to use the letters provided.")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Misspelled", message: "The word you entered is not a real word.")
+            return
+        }
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
     }
 }
 
